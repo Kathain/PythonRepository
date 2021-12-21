7755881d829a7d95388c1c45f245d2f70f9746ac
Katha <kateryna.krasiuk@gmail.com>
2021-12-21 17:55:03 +0100

    Add new lessond

---
+++ BasicsPython/Lesson2.py
@@ -0,0 +1,7 @@
+age = 18
+if (age >= 25) :
+    print("OK")
+elif (age >= 18) and (age < 25) :
+    print("You can go with mom")
+else:
+    print("NO!")
---
+++ BasicsPython/Lesson3.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you?"))
+if (age >= 25) :
+    print("OK")
+elif (age >= 18) and (age < 25) :
+    print("You can go with mom")
+else:
+    print("NO!")
