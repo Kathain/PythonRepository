243287725ebcf02815c561fba6135eda75426f4f
Katha <kateryna.krasiuk@gmail.com>
2021-12-23 18:27:13 +0100

    new lessons

---
+++ BasicsPython/Lesson15.py
@@ -0,0 +1,8 @@
+spisok = []
+numbers = [3,4,5,9,10]
+print(numbers)
+
+numbers = [3, 5, 34.1, "name"]
+print(numbers)
+numbers = [3, 5, 34.1, [45, 7.1]]
+print(numbers)
---
+++ BasicsPython/Lesson18.py
@@ -0,0 +1,16 @@
+numbers = [4, 81, 96, 109, 87, 99, 61]
+
+
+numbers.sort()              # данный метод сортирует наши числа по возрастанию
+print(numbers)
+
+numbers.sort(reverse=True)         # отсортирует по убыванию
+print(numbers)
+
+numbers[3] = 'B'                # заменяет элемент по индексу на нужный нам (как напримпер на букву)
+print(numbers)
+
+letters = ['a','b','v', 'c']
+letters.sort()
+print(letters)
+
